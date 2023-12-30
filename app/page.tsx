074afb400594ce8a174bb29286e09225d9a671ed
Will <wseguin@uwo.ca>
2023-12-30 17:13:47 -0500

    navbar made - boostrap implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,34 @@
+import Navbar from "../components/Navbar";
 import { prisma } from "@/lib/prisma";
-
-export default async function Homer() {
-  const players = await prisma.PlayerStats.findMany({
-    where : {
-      Player: "Precious Achiuwa"
-    }
-  });
-
+import React from 'react';
+import '../components/MainPage.css'
+export default async function Home() {
   return (
-    <div>
-      {players.map((player, index) => (
-        <div key={index}>
-          <p>{player.Player}</p>
-          <p>{player.Pos}</p>
-          {/* Add more fields as needed */}
-        </div>
-      ))}
-    </div>
+    <main>
+      <Navbar />
+     <body>
+
+     </body>
+    </main>
   );
-}
+      }
+    
+  
+
+// const players = await prisma.PlayerStats.findMany({
+//   where : {
+//     Player: "Precious Achiuwa"
+//   }
+// });
+
+// return (
+//   <div>
+//     {players.map((player, index) => (
+//       <div key={index}>
+//         <p>{player.Player}</p>
+//         <p>{player.Pos}</p>
+//         {/* Add more fields as needed */}
+//       </div>
+//     ))}
+//   </div>
+// );
